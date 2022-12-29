b13b72121bd6903a165c4f05a5d3963a0fe87caf
petrovskinikolag@gmail.com <petrovskinikolag@gmail.com>
2022-12-29 10:57:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const account1 = {
 };
 
 const account2 = {
-  owner: "Jessica Davis",
+  owner: "Nikola G",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
